fd66aad10870848bf09618cdd5c456d2298eaf27
kishore kumar <114152420+kishorekumawath@users.noreply.github.com>
2026-02-14 22:30:11 +0530

    deployment config setup

---
+++ backend/src/lib/env.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const ENV = {
+    PORT: process.env.PORT,
+    DB_URL: process.env.DB_URL,
+    NODE_ENV: process.env.NODE_ENV
+}
+
+export default ENV;
---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import ENV from "./lib/env.js";
+import path from "path";
+
+const app = express();
+
+const __dirname = path.resolve();
+
+app.get("/test", (req, res) => {
+    res.status(200).json({
+        message: "this is the test endpoint"
+    })
+})
+
+app.get("/books", (req, res) => {
+    res.status(200).json({
+        message: "this is the books endpoint"
+    })
+})
+
+// make our app ready fro deployment
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`)
+})
