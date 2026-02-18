ce39fe1ccb1952e1dfbae7b89b0871fa03e18c75
kishore kumar <114152420+kishorekumawath@users.noreply.github.com>
2026-02-19 01:09:28 +0530

    auth and db sync

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,36 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import User from "../models/user.js";
+
+export const inngest = new Inngest({
+    name: "HireSync",
+});
+
+const syncUser = inngest.createFunction({
+    id: "Sync-user",
+    event: "clerk/user.created",
+    run: async ({ event }) => {
+        await connectDB();
+        const { id, email_addresses, first_name, last_name, image_url } = event.data;
+        const newUser = await User.create({
+            clerkId: id,
+            email: email_addresses[0]?.email_address,
+            name: `${first_name || ""} ${last_name || ""}`,
+            profileImage: image_url,
+        })
+
+        await User.create(newUser)
+    },
+});
+
+const deleteUser = inngest.createFunction({
+    id: "Delete-user",
+    event: "clerk/user.deleted",
+    run: async ({ event }) => {
+        await connectDB();
+        const { id } = event.data;
+        await User.deleteOne({ clerkId: id })
+    },
+});
+
+export const functions = [syncUser, deleteUser];
