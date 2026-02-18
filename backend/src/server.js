b6c9d9def02a5d458084930ca1849df29596ebf5
kishore kumar <114152420+kishorekumawath@users.noreply.github.com>
2026-02-19 01:30:16 +0530

    imported the cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import ENV from "./lib/env.js";
 import path from "path";
 import connectDB from "./lib/db.js";
+import cors from "cors";
 import { serve } from "inngest/express";
 import { inngest } from "./lib/inngest.js";
 import { functions } from "./lib/inngest.js";
